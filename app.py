8ce2a74fd9b5574a47c757697f7e7e507925f500
Kai Devrim <kkgit@devrim.tech>
2021-11-13 20:38:29 -0800

    Setup Auth

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from gphotospy import authorize
+from gphotospy.album import *
+
 
 app = Flask(__name__)
-
+CLIENT_SECRET_FILE = "./gphoto_oauth.json"
+service = authorize.init(CLIENT_SECRET_FILE)
 
 @app.route('/')
 def hello():
